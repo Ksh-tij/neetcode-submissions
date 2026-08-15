31e9616d501530bb4252ac99c6224af04a1b1f45
Ksh-tij <145128793+Ksh-tij@users.noreply.github.com>
2026-08-15 13:42:21 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]"""
+        hmap = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in hmap:
+                return [hmap[diff],i]
+            hmap[n] = i
+        return
+        
