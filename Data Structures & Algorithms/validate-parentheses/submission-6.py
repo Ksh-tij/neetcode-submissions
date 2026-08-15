53f12d38e788fefa73b0842852f6235042e41596
Ksh-tij <145128793+Ksh-tij@users.noreply.github.com>
2026-08-15 19:06:52 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        hmap = { ")":"(","]":"[","}":"{"}
+        for c in s:
+            if c in hmap:
+                if stk and stk[-1] == hmap[c]:
+                    stk.pop()
+                    continue
+                else:
+                    return False
+            else:
+                stk.append(c)
+        return not stk
+        
